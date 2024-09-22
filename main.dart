9a0e32afb153d21926ac83c3487088205e31485b
Gyasuddin <154743162+Gyasuddin0786@users.noreply.github.com>
2024-09-22 08:40:00 +0530

    Add files via upload
    This one page is a main dart page

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'package:bookapp/common/color_extenstion.dart';
+import 'package:bookapp/screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: TColor.primary,
+        fontFamily: 'SF Pro Text',
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
