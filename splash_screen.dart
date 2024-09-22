ecadf8bc52e5375b454b63ad77a7937ba33c7f9f
Gyasuddin <154743162+Gyasuddin0786@users.noreply.github.com>
2024-09-22 08:42:53 +0530

    Add files via upload
    This is a my first splash screen

---
+++ splash_screen.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'package:bookapp/view/onboarding/onboarding_view.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => const OnboardingView()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Book Store App',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: Colors.blue),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
